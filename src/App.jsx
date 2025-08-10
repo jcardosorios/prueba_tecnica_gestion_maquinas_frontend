e96b94b4b51a4e37518fce8c3b503a4c667b7348
Joaquin Cardoso <joaquin.cardoso.r@gmail.com>
2025-08-10 19:35:04 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-2xl'>Hola mundo</p>
+    </>
+  )
+}
+
+export default App
